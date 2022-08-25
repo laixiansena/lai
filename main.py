b0cbb68c67917853d2caec7d49e2ff46a92df232
赖先森 <111839339+laixiansena@users.noreply.github.com>
2022-08-25 17:50:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -219,14 +219,15 @@ if __name__ == "__main__":
     accessToken = get_access_token()
     # 接收的用户
     users = config["user"]
-    # 传入地区获取天气信息
+     # 传入地区获取天气信息
     region = config["region"]
     weather, temp, wind_dir = get_weather(region)
     note_ch = config["note_ch"]
-    if note_ch == "" :
+    note_en = config["note_en"]
+    if note_ch == "" and note_en == "":
         # 获取词霸每日金句
-        note_ch = get_ciba()
+        note_ch, note_en = get_ciba()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, )
+        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
     os.system("pause")
